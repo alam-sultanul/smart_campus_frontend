8a8826392c818d629b49064101697b6a915ef024
Your Name <sultanulalam21@gmail.com>
2021-01-23 03:54:15 +0600

    complete search filter

--- src/app/home/leadlist/showlead/showlead.component.ts
+++ src/app/home/leadlist/showlead/showlead.component.ts
@@ -8,85 +8,131 @@ import { UserService } from 'src/app/shared/user.service';
 })
 export class ShowleadComponent implements OnInit {
 
-  
-
-  
-  constructor(private service:UserService) { }
-
- ProfileList:any=[];
- ActivateAddEditDeleteComp:boolean=false;
- ModalTitle!:string;
- 
-  pro:any;
 
 
-  ngOnInit(): void 
-  {
+
+  constructor(private service: UserService) { }
+
+  ProfileList: any = [];
+  ActivateAddEditDeleteComp: boolean = false;
+  ModalTitle!: string;
+
+
+
+
+  pro: any;
+
+
+
+  //for filtering
+  profileNameFilter: string = "";
+  originTypeFilter: string = "";
+  leadScoringFilter: string = "";
+  registrationDatefilter:any;
+  leadlistingListWithOutFilter: any = [];
+
+
+
+  ngOnInit(): void {
     this.refreshProfileList();
   }
 
-  refreshProfileList()
+  refreshProfileList() {
+
+    this.service.getLeadListProfileList().subscribe
+      (
+        data => {
+          this.ProfileList = data;
+          this.leadlistingListWithOutFilter = data;
+        }
+      );
+
+  }
+
+  FilterFn() 
   {
 
-    this.service.getLeadListProfileList().subscribe(data=>{this.ProfileList=data;});
+    var profileNameFilter=this.profileNameFilter;
+    var originTypeFilter=this.originTypeFilter;
+    var leadScoringFilter=this.leadScoringFilter;
     
-  }
 
-  addClick()
-  {
-    alert("modal add button");
-    this.pro={
-      ProfileId:0,
-      ProfileName:"",
-      OriginType:"",
-      EmailAddress:"",
-      ContactNumber:"",
-      Nationality:"",
-      LeadScoring:"",
-      LeadStatus:"",
-      ProfileType:"",
-      RegistrationDate:""
-    }
-    this.ModalTitle="Add New Profile";
-    this.ActivateAddEditDeleteComp=true;
+    this.ProfileList=this.leadlistingListWithOutFilter.filter(function(el){
+      return el.ProfileName.toString().toLowerCase().includes(
+        profileNameFilter.toString().trim().toLowerCase()
     
-    this.refreshProfileList();
-  }
-
-  editClick(dataItem:any)
-  {
-    alert("Edit Profile Name : "+dataItem.ProfileName);
-    console.log(dataItem);
-    this.pro=dataItem;
-    alert("Pro Name : "+dataItem.ProfileId);
-    this.ModalTitle="Edit Profile";
-    this.ActivateAddEditDeleteComp=true;
-
-  }
-
-  closeClick()
-  {
-    this.refreshProfileList();
-    this.ActivateAddEditDeleteComp=false;
-
-  }
-
-  deleteClick(item:any)
-  {
-    if(confirm('Are you sure Delete '+item.ProfileName+' profile'))
-    {
-      this.service.deleteLeadListProfile(item.ProfileId).subscribe(data=>{alert(data.toString())
-      this.refreshProfileList();
+       ) &&
+        el.OriginType.toString().toLowerCase().includes(
+        originTypeFilter.toString().trim().toLowerCase()
+       
+        )&&
+        el.LeadScoring.toString().toLowerCase().includes(
+        leadScoringFilter.toString().trim(). toLowerCase() 
+        )
 
     });
-      
+
+
+
+  }
+
+  searchdate()
+  {
+
+  }
+
+  addClick() {
+    alert("modal add button");
+    this.pro = {
+      ProfileId: 0,
+      ProfileName: "",
+      OriginType: "",
+      EmailAddress: "",
+      ContactNumber: "",
+      Nationality: "",
+      LeadScoring: "",
+      LeadStatus: "",
+      ProfileType: "",
+      RegistrationDate: ""
+    }
+    this.ModalTitle = "Add New Profile";
+    this.ActivateAddEditDeleteComp = true;
+
+    this.refreshProfileList();
+  }
+
+  editClick(dataItem: any) {
+    alert("Edit Profile Name : " + dataItem.ProfileName);
+    console.log(dataItem);
+    this.pro = dataItem;
+    alert("Pro Name : " + dataItem.ProfileId);
+    this.ModalTitle = "Edit Profile";
+    this.ActivateAddEditDeleteComp = true;
+
+  }
+
+  closeClick() {
+    this.refreshProfileList();
+    this.ActivateAddEditDeleteComp = false;
+
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Are you sure Delete ' + item.ProfileName + ' profile')) {
+      this.service.deleteLeadListProfile(item.ProfileId).subscribe(data => {
+        alert(data.toString())
+        this.refreshProfileList();
+
+      });
+
     }
 
   }
 
-  refreshClick()
-  {
+  refreshClick() {
     alert('refresh');
     this.refreshProfileList();
   }
+
+
 }
